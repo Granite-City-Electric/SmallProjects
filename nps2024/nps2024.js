8eabca3808d36646f7cd0c170f9e289c5aaf0a39
mizzdawg <mcarty79@gmail.com>
2024-04-08 16:12:46 -0400

    Add JS for popup functionality on NPS 2024 web page

--- nps2024/nps2024.js
+++ nps2024/nps2024.js
@@ -1,1 +1,15 @@
-console.log('test');
+const logos = document.querySelectorAll(".gce-logo-ph");
+const popups = document.querySelectorAll(".gce-pu-container");
+const closeouts = document.querySelectorAll(".gce-pu-closeout-container")
+
+logos.forEach(function(el, idx) {
+    el.addEventListener("click", function() {
+        popups[idx].classList.toggle("gce-pu-hide");
+    })
+})
+
+closeouts.forEach(function(x, idx) {
+    x.addEventListener("click", function () {
+        popups[idx].classList.toggle("gce-pu-hide");
+    })
+})
