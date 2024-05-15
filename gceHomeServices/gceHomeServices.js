d3d9558246b6c90f7623ce320910269e5b8dba2d
mizzdawg <mcarty79@gmail.com>
2024-05-15 15:17:41 -0400

    Add JS to control info panel on GCE Home Services Generators page

--- gceHomeServices/gceHomeServices.js
+++ gceHomeServices/gceHomeServices.js
@@ -3,4 +3,21 @@ const links = document.querySelector('.gce-hs-nav-links-mobile-container');
 
 btn.addEventListener('click', () => {
     links.classList.toggle('gce-hs-hide-mobile-menu')
+});
+
+
+const genTabs = document.querySelectorAll('.gce-hs-g-panel-control');
+const genTabHeaders = document.querySelectorAll('.gce-hs-g-control-header');
+const genPanels = document.querySelectorAll('.gce-hs-g-panel-info');
+
+genTabs.forEach((el,idx) => {
+    el.addEventListener("click", function() {
+        genTabs.forEach((tab) => tab.classList.remove('gce-hs-g-panel-control-active'));
+        genTabHeaders.forEach((head) => head.classList.remove('gce-hs-g-panel-control-header-active'));
+        genPanels.forEach((panel) => panel.classList.remove('gce-hs-g-panel-info-active'))
+        
+        genTabs[idx].classList.add('gce-hs-g-panel-control-active');
+        genTabHeaders[idx].classList.add('gce-hs-g-panel-control-header-active');
+        genPanels[idx].classList.add('gce-hs-g-panel-info-active');
+    })
 })
