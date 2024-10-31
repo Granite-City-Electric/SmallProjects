66a4ffbb2af2b9f02dd8f0ce42b2451329e14881
mizzdawg <mcarty79@gmail.com>
2024-10-31 12:04:39 -0400

    Add overlay to top nav hover effect

--- topNavRedesign/topNavRedesign.js
+++ topNavRedesign/topNavRedesign.js
@@ -1,6 +1,7 @@
 const dropdownLinks = document.querySelectorAll('.has-dropdown');
+const overlay = document.querySelector('.overlay');
 
-function toggleDrop(el) {
+function toggleShow(el) {
     if (el.classList.contains('show-flyout')) {
         el.classList.remove('show-flyout');
         el.classList.add('hide-flyout');
@@ -13,13 +14,20 @@ function toggleDrop(el) {
 dropdownLinks.forEach((el) => {
     el.addEventListener('mouseenter', (e) => {
         const flyout = el.querySelector('.header-section-3-flyout');
-        toggleDrop(flyout);
+        toggleShow(flyout);
+        toggleShow(overlay);
     })
 })
 
 dropdownLinks.forEach((el) => {
     el.addEventListener('mouseleave', (e) => {
         const flyout = el.querySelector('.header-section-3-flyout');
-        toggleDrop(flyout);
+        toggleShow(flyout);
+        toggleShow(overlay);
     })
-})
+});
+
+function toggleOverlay() {
+    toggleShow(overlay);
+}
+
